8f349ed789b93cd234e1380c9a4cc47c1705ba0d
Ehsan <ehsansouri23@gmail.com>
2018-07-27 00:02:43 +0430

    saving and getting media from db

--- app/src/main/java/com/usermanual/helper/DataBaseHelper.java
+++ app/src/main/java/com/usermanual/helper/DataBaseHelper.java
@@ -2,12 +2,16 @@ package com.usermanual.helper;
 
 import android.content.Context;
 
+import com.usermanual.model.Media;
 import com.usermanual.model.Title;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.usermanual.helper.PrefHelper.*;
+import static com.usermanual.helper.PrefHelper.getInt;
+import static com.usermanual.helper.PrefHelper.getString;
+import static com.usermanual.helper.PrefHelper.saveInt;
+import static com.usermanual.helper.PrefHelper.saveString;
 
 public class DataBaseHelper {
 
@@ -16,12 +20,29 @@ public class DataBaseHelper {
             saveString(context, i + 1 + "", titleList.get(i).getTitle());
             for (int j = 0; j < titleList.get(i).getSubTitles().size(); j++) {
                 saveString(context, generateSubTitlesKey(i + 1, j + 1), titleList.get(i).getSubTitles().get(j).getSubTitle());
+                for (int k = 0; k < titleList.get(i).getSubTitles().get(j).getMedias().size(); k++) {
+                    saveString(context, generateMediaTextKey(i + 1, j + 1, k + 1), titleList.get(i).getSubTitles().get(j).getMedias().get(k).getText());
+                    saveString(context, generateMediaUrlKey(i + 1, j + 1, k + 1), titleList.get(i).getSubTitles().get(j).getMedias().get(k).getUrl());
+                    saveInt(context, generateMediaTypeKey(i + 1, j + 1, k + 1), titleList.get(i).getSubTitles().get(j).getMedias().get(k).getType());
+                }
             }
         }
     }
 
     private static String generateSubTitlesKey(int title, int subTitle) {
         return title + "_" + subTitle;
+    }
+
+    private static String generateMediaTextKey(int title, int subTitle, int media) {
+        return title + "_" + subTitle + "_" + media + "_T";
+    }
+
+    private static String generateMediaUrlKey(int title, int subTitle, int media) {
+        return title + "_" + subTitle + "_" + media + "_U";
+    }
+
+    private static String generateMediaTypeKey(int title, int subTitle, int media) {
+        return title + "_" + subTitle + "_" + media + "_Y";
     }
 
     public static List<String> getTitlesList(Context context) {
@@ -48,6 +69,23 @@ public class DataBaseHelper {
             subtitle = getString(context, key, null);
         }
         return subtitles;
+    }
+
+    public static List<Media> getMediaList(Context context, int title, int subTitle) {
+        List<Media> mediaList = new ArrayList<>();
+        int i = 1;
+        String key = generateMediaTextKey(title, subTitle, i);
+        String media = getString(context, key, null);
+        while (media != null) {
+            Media media1 = new Media(getString(context, generateMediaTextKey(title, subTitle, i), null)
+                    , getString(context, generateMediaUrlKey(title, subTitle, i), null),
+                    getInt(context, generateMediaTypeKey(title, subTitle, i), -1));
+            mediaList.add(media1);
+            i++;
+            key = generateMediaTextKey(title, subTitle, i);
+            media = getString(context, key, null);
+        }
+        return mediaList;
     }
 
 }
