46c27cc091b729b7360f393077dc536033ba4219
Ehsan <ehsansouri23@gmail.com>
2018-07-26 15:56:22 +0430

    essential prefs added

---
+++ app/src/main/java/com/usermanual/helper/PrefHelper.java
@@ -0,0 +1,28 @@
+package com.usermanual.helper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrefHelper {
+
+    private static final String PREFS = "prefs";
+
+    private static SharedPreferences prefs;
+
+    private static SharedPreferences getPrefs(Context context) {
+        if (prefs == null)
+            prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+        return prefs;
+    }
+
+    public static void saveString(Context context, String key, String value) {
+        SharedPreferences.Editor editor = getPrefs(context).edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getString(Context context, String key, String def) {
+        SharedPreferences prefs = getPrefs(context);
+        return prefs.getString(key, def);
+    }
+}
