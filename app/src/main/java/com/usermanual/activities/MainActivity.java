a31e336cef73c175296c5767153e8ff9627f3df3
Ehsan <ehsansouri23@gmail.com>
2018-07-27 00:22:12 +0430

    handling going to media activity

--- app/src/main/java/com/usermanual/activities/MainActivity.java
+++ app/src/main/java/com/usermanual/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.usermanual.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +14,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.usermanual.R;
 import com.usermanual.helper.DataBaseHelper;
+import com.usermanual.model.Media;
 import com.usermanual.model.Title;
 
 import java.util.List;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     int state;
+    int title, subTitle;
+
     ListView titlesListView;
     ArrayAdapter<String> adapter;
 
@@ -38,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
         arrow = (TextView) findViewById(R.id.arrow);
         subtitleGuide = (TextView) findViewById(R.id.subtitle_guide);
 
+//        for (int i = 0; i < 100; i++) { //todo delete it
+//            DownloadClass downloadClass = new DownloadClass();
+//            downloadClass.execute(i + "");
+//        }
+
 
         String json = "[\n" +
                 "  {\n" +
@@ -47,7 +57,17 @@ public class MainActivity extends AppCompatActivity {
                 "        \"subTitle\": \"subtitle 1 1\",\n" +
                 "        \"medias\": [\n" +
                 "          {\n" +
-                "            \"text\": \"text1\",\n" +
+                "            \"text\": \"text 1\",\n" +
+                "            \"url\": \"url 1\",\n" +
+                "            \"type\": 1\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"text\": \"text 2\",\n" +
+                "            \"url\": \"url 1\",\n" +
+                "            \"type\": 1\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"text\": \"text 3\",\n" +
                 "            \"url\": \"url 1\",\n" +
                 "            \"type\": 1\n" +
                 "          }\n" +
@@ -159,6 +179,7 @@ public class MainActivity extends AppCompatActivity {
                 switch (state) {
                     case 0:
                         state = 1;
+                        title = position + 1;
                         titleGuide.setText(titleList.get(position));
                         titleGuide.setVisibility(View.VISIBLE);
                         List<String> subtitles = DataBaseHelper.getSubtitleList(getApplicationContext(), position + 1);
@@ -168,13 +189,27 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case 1:
                         state = 2;
+                        subTitle = position + 1;
                         arrow.setVisibility(View.VISIBLE);
                         subtitleGuide.setText(adapter.getItem(position));
                         subtitleGuide.setVisibility(View.VISIBLE);
+
+                    case 2:
+                        List<Media> mediaList = DataBaseHelper.getMediaList(getApplicationContext(), title, subTitle);
+                        Intent intent = new Intent(MainActivity.this, MediaActivity.class);
+                        Log.e(TAG, "onItemClick: " + mediaList.size());
+                        startActivity(intent);
                         break;
                 }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (state == 2)
+            state = 1;
     }
 
     @Override
