6421dae183ac489215cf41ee7f6784b7b01150af
Ehsan <ehsansouri23@gmail.com>
2018-07-26 20:33:31 +0430

    guides added to main activity.

--- app/src/main/java/com/usermanual/activities/MainActivity.java
+++ app/src/main/java/com/usermanual/activities/MainActivity.java
@@ -1,14 +1,12 @@
 package com.usermanual.activities;
 
-import android.content.Intent;
-import android.provider.ContactsContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -21,14 +19,24 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    int state;
     ListView titlesListView;
+    ArrayAdapter<String> adapter;
+
+    TextView titleGuide, arrow, subtitleGuide;
+
+    List<String> titleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        state = 0;
         titlesListView = (ListView) findViewById(R.id.titles_list_view);
+        titleGuide = (TextView) findViewById(R.id.title_guide);
+        arrow = (TextView) findViewById(R.id.arrow);
+        subtitleGuide = (TextView) findViewById(R.id.subtitle_guide);
 
 
         String json = "[\n" +
@@ -136,10 +144,11 @@ public class MainActivity extends AppCompatActivity {
                 "]";
 
         Gson gson = new Gson();
-        List<Title> titles = gson.fromJson(json, new TypeToken<List<Title>>(){}.getType());
+        List<Title> titles = gson.fromJson(json, new TypeToken<List<Title>>() {
+        }.getType());
         DataBaseHelper.saveToDB(getApplicationContext(), titles);
-        List<String> titleList = DataBaseHelper.getTitlesList(getApplicationContext());
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        titleList = DataBaseHelper.getTitlesList(getApplicationContext());
+        adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, titleList);
         adapter.notifyDataSetChanged();
         titlesListView.setAdapter(adapter);
@@ -147,10 +156,38 @@ public class MainActivity extends AppCompatActivity {
         titlesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, SubtitlesActivity.class);
-                intent.putExtra("title", position + 1);
-                startActivity(intent);
+                switch (state) {
+                    case 0:
+                        state = 1;
+                        titleGuide.setText(titleList.get(position));
+                        titleGuide.setVisibility(View.VISIBLE);
+                        List<String> subtitles = DataBaseHelper.getSubtitleList(getApplicationContext(), position + 1);
+                        adapter.clear();
+                        adapter.addAll(subtitles);
+                        adapter.notifyDataSetChanged();
+                        break;
+                    case 1:
+                        state = 2;
+                        arrow.setVisibility(View.VISIBLE);
+                        subtitleGuide.setText(adapter.getItem(position));
+                        subtitleGuide.setVisibility(View.VISIBLE);
+                        break;
+                }
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (state == 0)
+            super.onBackPressed();
+        if (state == 1) {
+            titleList = DataBaseHelper.getTitlesList(getApplicationContext());
+            adapter.clear();
+            adapter.addAll(titleList);
+            adapter.notifyDataSetChanged();
+            titleGuide.setVisibility(View.GONE);
+            state = 0;
+        }
     }
 }
