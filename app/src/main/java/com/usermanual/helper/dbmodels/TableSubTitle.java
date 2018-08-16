c2a26ba50463b72478debc53b24f63e0801ed1da
Ehsan <ehsansouri23@gmail.com>
2018-08-16 15:26:01 +0430

    table models implements parcalable

--- app/src/main/java/com/usermanual/helper/dbmodels/TableSubTitle.java
+++ app/src/main/java/com/usermanual/helper/dbmodels/TableSubTitle.java
@@ -2,11 +2,15 @@ package com.usermanual.helper.dbmodels;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
+
 @Entity
-public class TableSubTitle {
+public class TableSubTitle implements Serializable, Parcelable {
 
     @PrimaryKey(autoGenerate = true)
     public int id;
@@ -21,4 +25,42 @@ public class TableSubTitle {
 
     @SerializedName("Title")
     public String subtitle;
+
+    public TableSubTitle() {
+
+    }
+
+    protected TableSubTitle(Parcel in) {
+        id = in.readInt();
+        titleId = in.readInt();
+        subtitleId = in.readInt();
+        title = in.readString();
+        subtitle = in.readString();
+    }
+
+    public static final Creator<TableSubTitle> CREATOR = new Creator<TableSubTitle>() {
+        @Override
+        public TableSubTitle createFromParcel(Parcel in) {
+            return new TableSubTitle(in);
+        }
+
+        @Override
+        public TableSubTitle[] newArray(int size) {
+            return new TableSubTitle[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeInt(titleId);
+        dest.writeInt(subtitleId);
+        dest.writeString(title);
+        dest.writeString(subtitle);
+    }
 }
--- app/src/main/java/com/usermanual/helper/dbmodels/TableTitle.java
+++ app/src/main/java/com/usermanual/helper/dbmodels/TableTitle.java
@@ -2,11 +2,15 @@ package com.usermanual.helper.dbmodels;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
+
 @Entity
-public class TableTitle {
+public class TableTitle implements Serializable, Parcelable {
 
     @PrimaryKey(autoGenerate = true)
     public int id;
@@ -16,4 +20,38 @@ public class TableTitle {
 
     @SerializedName("Title")
     public String title;
+
+    public TableTitle() {
+
+    }
+
+    protected TableTitle(Parcel in) {
+        id = in.readInt();
+        titleId = in.readInt();
+        title = in.readString();
+    }
+
+    public static final Creator<TableTitle> CREATOR = new Creator<TableTitle>() {
+        @Override
+        public TableTitle createFromParcel(Parcel in) {
+            return new TableTitle(in);
+        }
+
+        @Override
+        public TableTitle[] newArray(int size) {
+            return new TableTitle[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeInt(titleId);
+        dest.writeString(title);
+    }
 }
