4c7d8d9f07164cf671948a252e7e6881a2547261
Ehsan <ehsansouri23@gmail.com>
2018-08-17 23:21:08 +0430

    retrofit classes added

---
+++ app/src/main/java/com/usermanual/network/GetData.java
@@ -0,0 +1,19 @@
+package com.usermanual.network;
+
+import com.usermanual.helper.dbmodels.TableSubTitle;
+import com.usermanual.helper.dbmodels.TableTitle;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
+public interface GetData {
+
+    @GET("titles")
+    Call<List<TableTitle>> getTitles();
+
+    @GET()
+    Call<List<TableSubTitle>> getSubtitles(@Url String s);
+}
