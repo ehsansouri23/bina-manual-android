b69f4252eefacefe87d6cd75b04bc8a7b9604139
Ehsan <ehsansouri23@gmail.com>
2018-09-02 01:10:28 +0430

    news model updated

---
+++ app/src/main/java/com/usermanual/helper/dbmodels/NewsModel.java
@@ -0,0 +1,15 @@
+package com.usermanual.helper.dbmodels;
+
+import com.google.gson.annotations.SerializedName;
+
+public class NewsModel {
+
+    @SerializedName("ID")
+    public int newsId;
+
+    @SerializedName("Title")
+    public String title;
+
+    @SerializedName("Url")
+    public String url;
+}
