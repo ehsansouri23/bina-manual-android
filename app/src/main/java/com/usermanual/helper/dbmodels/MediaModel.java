06046778695bdc229f3ef5d7396fb4003eff7212
Ehsan <ehsansouri23@gmail.com>
2018-09-01 23:59:13 +0430

    media model added

---
+++ app/src/main/java/com/usermanual/helper/dbmodels/MediaModel.java
@@ -0,0 +1,28 @@
+package com.usermanual.helper.dbmodels;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MediaModel {
+
+    @SerializedName("SubtitleID")
+    public int subtitleId;
+
+    @SerializedName("Title")
+    public String title;
+
+    @SerializedName("Medias")
+    public List<Media> medias;
+
+    class Media {
+        @SerializedName("Text")
+        public String text;
+
+        @SerializedName("Url")
+        public String url;
+
+        @SerializedName("Type")
+        public int type;
+    }
+}
