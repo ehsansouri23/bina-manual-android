0b8f6329c85cd394cdb0ee1d689a8cebedc210c8
Ehsan <ehsansouri23@gmail.com>
2018-09-20 23:02:59 +0430

    some codes cleaned

--- app/src/main/java/com/usermanual/helper/StorageHelper.java
+++ app/src/main/java/com/usermanual/helper/StorageHelper.java
@@ -8,21 +8,6 @@ import static com.usermanual.helper.Consts.*;
 
 public class StorageHelper {
 
-    public static File getFile(Context context, String fileName) {
-        File baseFile = context.getDir("hello", Context.MODE_PRIVATE);
-        File dir = new File(baseFile, fileName);
-        if (!dir.exists())
-            dir.mkdirs();
-        return dir;
-    }
-
-    public static void deleteFile(Context context, String fileName) {
-        File baseFile = getFile(context, fileName);
-        File file = new File(baseFile, fileName);
-        if (file.exists())
-            file.delete();
-    }
-
     public static class FileSpec {
         String fileKey;
         String url;
