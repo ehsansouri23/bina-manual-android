4ac4f33cb0341b01af187aac668463d891b94b50
Ehsan <ehsansouri23@gmail.com>
2018-09-27 22:54:41 +0330

    DONE

--- app/src/main/java/com/usermanual/activities/WebViewActivity.java
+++ app/src/main/java/com/usermanual/activities/WebViewActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
 import com.usermanual.R;
 
 import static com.usermanual.helper.Consts.*;
@@ -13,7 +15,9 @@ public class WebViewActivity extends AppCompatActivity {
     private static final String TAG = "WebViewActivity";
 
     String fullHtml;
+    String imageKey;
     WebView webView;
+    ImageView image;
     final String mimeType = "text/html";
     final String encoding = "UTF-8";
 
@@ -21,7 +25,10 @@ public class WebViewActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview);
+        image = (ImageView) findViewById(R.id.news_image);
         fullHtml = getIntent().getStringExtra(NEWS_FULL_HTML);
+        imageKey = getIntent().getStringExtra(NEWS_IMAGE_KEY);
+        Picasso.get().load(BASE_URL + FILE_URL + imageKey).placeholder(R.mipmap.car).into(image);
         webView = (WebView) findViewById(R.id.webview);
         webView.loadDataWithBaseURL("", fullHtml, mimeType, encoding, "");
     }
