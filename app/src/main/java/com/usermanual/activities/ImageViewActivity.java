b9ffb5616c34e53f6704a642d0f90dce3b41ac62
Ehsan <ehsansouri23@gmail.com>
2018-10-14 20:11:59 +0330

    zoom in image view

--- app/src/main/java/com/usermanual/activities/ImageViewActivity.java
+++ app/src/main/java/com/usermanual/activities/ImageViewActivity.java
@@ -1,11 +1,12 @@
 package com.usermanual.activities;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
+import android.webkit.WebView;
 
-import com.squareup.picasso.Picasso;
 import com.usermanual.R;
 import com.usermanual.helper.StorageHelper;
 
@@ -14,16 +15,20 @@ import static com.usermanual.helper.Consts.FILE_KEY;
 public class ImageViewActivity extends AppCompatActivity {
     private static final String TAG = "ImageViewActivity";
 
+    Context context;
     String fileKey;
-    ImageView image;
+    WebView image;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imageview);
-        image = (ImageView) findViewById(R.id.image);
+        context = getApplicationContext();
+        image = (WebView) findViewById(R.id.image);
 
         fileKey = getIntent().getExtras().getString(FILE_KEY);
-        Picasso.get().load(StorageHelper.getFile(getApplicationContext(), fileKey)).into(image);
+        Uri imageUri = Uri.fromFile(StorageHelper.getFile(context, fileKey));
+        image.getSettings().setBuiltInZoomControls(true);
+        image.loadUrl(imageUri.toString());
     }
 }
