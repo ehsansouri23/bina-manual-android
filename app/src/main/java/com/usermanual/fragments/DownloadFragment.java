a260068a44459e7969324d52872f96d90e77a559
Ehsan <ehsansouri23@gmail.com>
2018-10-14 20:18:06 +0330

    no item added in download

--- app/src/main/java/com/usermanual/fragments/DownloadFragment.java
+++ app/src/main/java/com/usermanual/fragments/DownloadFragment.java
@@ -9,13 +9,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.usermanual.R;
 import com.usermanual.adapter.DownloadAdapter;
+import com.usermanual.helper.DataBaseHelper;
 
 public class DownloadFragment extends Fragment {
     private static final String TAG = "DownloadFragment";
 
+    LinearLayout noItems;
     RecyclerView list;
     DownloadAdapter downloadAdapter;
 
@@ -23,7 +26,14 @@ public class DownloadFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.search_fragment, container, false);
+        noItems = (LinearLayout) view.findViewById(R.id.no_item);
         list = (RecyclerView) view.findViewById(R.id.list);
+        if (DataBaseHelper.getToDownloadFiles(getContext()).isEmpty()) {
+            noItems.setVisibility(View.VISIBLE);
+            return view;
+        }
+        else
+            noItems.setVisibility(View.GONE);
         downloadAdapter = new DownloadAdapter(getContext());
         list.setAdapter(downloadAdapter);
         list.setLayoutManager(new LinearLayoutManager(getContext()));
