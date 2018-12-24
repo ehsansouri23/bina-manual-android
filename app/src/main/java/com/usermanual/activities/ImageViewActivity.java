99d857a8a06586d94590ef0176d75b49d70ed597
Ehsan <ehsansouri23@gmail.com>
2018-12-25 00:02:11 +0330

    add some logs

--- app/src/main/java/com/usermanual/activities/ImageViewActivity.java
+++ app/src/main/java/com/usermanual/activities/ImageViewActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.WebView;
 
 import com.usermanual.R;
@@ -31,12 +32,15 @@ public class ImageViewActivity extends AppCompatActivity {
         fileKey = getIntent().getExtras().getString(FILE_KEY);
         fileUrl = getIntent().getExtras().getString(FILE_URL);
         if (fileKey != null) {
+            Log.e(TAG, "onCreate: file key: " + fileKey);
             Uri imageUri = Uri.fromFile(StorageHelper.getFile(context, fileKey));
             image.getSettings().setBuiltInZoomControls(true);
             image.loadUrl(imageUri.toString());
         } else if (fileUrl != null) {
+            Log.e(TAG, "onCreate: file url: " + fileUrl);
             image.getSettings().setBuiltInZoomControls(true);
-            image.loadUrl(fileUrl);
+//            image.loadUrl(fileUrl);
+            image.loadDataWithBaseURL(null, "<html><head></head><body><img style=\"margin: 7px 7px\" src=\"" + fileUrl + "\"></body></html>", "html/css", "utf-8", null);
         }
     }
 }
